3f98899b57de53ddce43c5d2c4b4a2d20b14a135
yueyi <1481796592@qq.com>
2024-06-12 15:47:34 +0800

    swap chain

--- VulkanTest/headers/render.h
+++ VulkanTest/headers/render.h
@@ -4,9 +4,12 @@
 
 #include <iostream>
 #include <stdexcept>
+#include <algorithm>
 #include <vector>
 #include <cstring>
 #include <cstdlib>
+#include <cstdint>
+#include <limits>
 #include <optional>
 #include <set>
 
@@ -16,6 +19,10 @@ namespace myrender {
 
 	const std::vector<const char*> validationLayers = {
 	"VK_LAYER_KHRONOS_validation"
+	};
+
+	const std::vector<const char*> deviceExtensions = {
+		VK_KHR_SWAPCHAIN_EXTENSION_NAME
 	};
 
 #ifdef NDEBUG
@@ -31,6 +38,12 @@ namespace myrender {
 		bool isComplete() {
 			return graphicsFamily.has_value() && presentFamily.has_value();
 		}
+	};
+
+	struct SwapChainSupportDetails {
+		VkSurfaceCapabilitiesKHR capabilities;
+		std::vector<VkSurfaceFormatKHR> formats;
+		std::vector<VkPresentModeKHR> presentModes;
 	};
 
 	class RenderingApp
@@ -55,6 +68,11 @@ namespace myrender {
 		VkQueue graphicsQueue;
 		VkQueue presentQueue;
 
+		VkSwapchainKHR swapChain;
+		std::vector<VkImage> swapChainImages;
+		VkFormat swapChainImageFormat;
+		VkExtent2D swapChainExtent;
+
 		void initWindow();
 		void initVulkan();
 		void mainLoop();
@@ -65,7 +83,13 @@ namespace myrender {
 		void createSurface();
 		void pickPhysicalDevice();
 		void createLogicalDevice();
+		void createSwapChain();
+		VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
+		VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
+		VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
+		SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
 		bool isDeviceSuitable(VkPhysicalDevice device);
+		bool checkDeviceExtensionSupport(VkPhysicalDevice device);
 		QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
 		std::vector<const char*> getRequiredExtensions();
 		bool checkValidationLayerSupport();
